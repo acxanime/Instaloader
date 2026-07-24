616490bda1481de43c5e1b2bc7c3494d6d80abc8
acxanime <acxanime07@gmail.com>
2026-07-24 22:36:04 +0530

    Update var.py

--- var.py
+++ var.py
@@ -9,7 +9,7 @@ ADMIN = int(os.getenv("ADMIN", "6692613520"))
 
 CHNL_LINK = os.getenv("CHNL_LINK", "https://t.me/ST_Rename_Update")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002320080278"))
-DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002956078693"))
+DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002451946366"))
 
 DB_URI = os.getenv("DB_URI", "mongodb+srv://sitaratoons:sitaratoons@cluster0.98nq3.mongodb.net/?retryWrites=true&w=majority") #MongoDB URL
 DB_NAME = os.getenv("DB_NAME", "Cluster0")
